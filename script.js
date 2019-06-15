f354c714ac5f9a8372eaa0486c8b1f7fb1b5fc8f
dock-cyber <51847952+dock-cyber@users.noreply.github.com>
2019-06-15 15:45:06 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var penumpang = ['Fikri', undefined, 'Safli'];
+var penumpang = ['Salim', undefined, 'Rafli'];
 var tambahPenumpang = function (namaPenumpang, penumpang){
     // Jika angkot kosong
     if(penumpang.length == 0){
@@ -10,17 +10,19 @@ var tambahPenumpang = function (namaPenumpang, penumpang){
     else{
         // telusuri seluruh kursi dari awal
         for(var i = 0; i< penumpang.length ; i++){
-            // jika ada kursi kosong
-            if(penumpang[i] == undefined){
-                // tambah penumpang di kursi tersebut
-                penumpang[i] = namaPenumpang;
+            // jika ada nama yang sama
+            if(penumpang[i] == namaPenumpang){
+                // tambil pesan error
+                console.log(namaPenumpang +' sudah ada di dalam angkot');
                 // kembalikan isi array & keluar dari function
                 return penumpang;
             }
-            // jika ada nama yang sama
-            else if(penumpang[i] == namaPenumpang){
-                // tambil pesan error
-                console.log(namaPenumpang +' sudah ada di dalam angkot');
+        }
+        for(var i = 0; i< penumpang.length ; i++){
+            // jika ada kursi kosong
+            if(penumpang[i] == undefined && namaPenumpang != penumpang[i] ){
+                // tambah penumpang di kursi tersebut
+                penumpang[i] = namaPenumpang;
                 // kembalikan isi array & keluar dari function
                 return penumpang;
             }
@@ -33,4 +35,34 @@ var tambahPenumpang = function (namaPenumpang, penumpang){
             }
         }
     }  
+}
+
+var hapusPenumpang = function(namaPenumpang, penumpang){
+    // jika angkot kosong 
+    if (penumpang.length == 0){
+        // tampilkan pesan angkot kosong
+        console.log('Angkot masih kosong bambang');
+        // kembalikan isi array dan keluar dari function
+        
+    }
+    else{
+        // telusuri kursi dari awal
+        for(var i = 0; i<penumpang.length; i++){
+            // jika nama sesuai
+            if(penumpang[i] == namaPenumpang){
+                // hapus nama menjadi undifined
+                penumpang[i] = undefined;
+                // kembalikan isi array dan keluar dari function
+                return penumpang;
+            }
+            // jika tidak ada nama yg sesuai
+            else if(i < namaPenumpang -1){
+                // tampilkan pesan kesalahannya
+                console.log(namaPenumpang+' tidak ada di daftar penumpang bambang!');
+                // kembalikan isi array dan keluar dari function
+                return penumpang;
+            }
+        }         
+    }
+    return penumpang;
 }
